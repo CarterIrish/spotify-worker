21b6e06354600846ef3ac194af8d4c5ba39d7888
Carter Irish <2023c.irish@gmail.com>
2025-09-08 21:01:44 -0400

    removed comment header to push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+
 
 // Replace these with your Spotify app credentials
 const CLIENT_ID = env.SPOTIFY_CLIENT_ID;
